38bf4501edbd5ecc710830cc9da488072c88cdd4
VSkoshchuk <skoschuk999@gmail.com>
2023-09-25 18:00:21 +0300

    SN-1038 Minor changes

--- examples/main.cpp
+++ examples/main.cpp
@@ -27,37 +27,37 @@ void socketPolling(Connection *connection) {
 }
 
 int main() {
-	cout << endl << "------------------- Creating requiered components -------------------" << endl;
+	cout << endl << "---- Creating requiered components ----" << endl;
 	Connection connection("56830", AF_INET);
 	ServerImpl server;
 	SecurityImpl security;
 	DeviceImpl device;
 
 	// Client initialization
-	cout << endl << "------------------- Creating WppClient -------------------" << endl;
+	cout << endl << "---- Creating WppClient ----" << endl;
 	WppClient::create({"Test name", "", ""}, connection);
 	WppClient *client = WppClient::takeOwnership();
 	WppRegistry &registry = client->registry();
 
 	// Initialize wpp objects
-	cout << endl << "------------------- Initialization wpp Server -------------------" << endl;
+	cout << endl << "---- Initialization wpp Server ----" << endl;
 	server.init(registry.server());
-	cout << endl << "------------------- Initialization wpp Security -------------------" << endl;
+	cout << endl << "---- Initialization wpp Security ----" << endl;
 	security.init(registry.security());
-	cout << endl << "------------------- Initialization wpp Device -------------------" << endl;
+	cout << endl << "---- Initialization wpp Device ----" << endl;
 	device.init(registry.device());
 	
 	// Giving ownership to registry
 	client->giveOwnership();
 
-	cout << endl << "------------------- Starting Connection thread -------------------" << endl;
+	cout << endl << "---- Starting Connection thread ----" << endl;
 	thread my_thread(socketPolling, &connection);
 
 	time_t callTime = 0;
 	for (int iterationCnt = 0; true; iterationCnt++) {
 		time_t currTime = time(NULL);
 
-		cout << endl << "------------------- iteration:" << iterationCnt << " -------------------" << endl;
+		cout << endl << "---- iteration:" << iterationCnt << " ----" << endl;
 		if (currTime >= callTime || connection.getPacketQueueSize()) {
 			// Handle client state and process packets from the server
 			client = WppClient::takeOwnership();

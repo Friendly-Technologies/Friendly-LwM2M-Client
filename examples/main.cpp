93a9f32f42f82c070be7c4efb48694f18f0e1c27
VSkoshchuk <skoschuk999@gmail.com>
2023-09-25 16:34:53 +0300

    SN-1038 Dropped unused function

--- examples/main.cpp
+++ examples/main.cpp
@@ -17,23 +17,7 @@
 #include "WppRegistry.h"
 
 using namespace std;
-
-void memConsumptionCheck() {
-//   std::shared_ptr<uint8_t> shared_array(new uint8_t[10], std::default_delete<uint8_t[]>());
-//   cout << "WppWppObject<Server>: " << sizeof(WppWppObject<Server>) << endl;
-//   cout << "WppWppObject<Server> serverObj: " << sizeof(serverObj) << endl;
-//   cout << "Server: " << sizeof(Server(1)) << endl;
-//   cout << "Resource: " << sizeof(Resource{}) << endl;
-//   cout << "Operation: " << sizeof(Operation{}) << endl;
-//   cout << "IS_MANDATORY: " << sizeof(IS_MANDATORY) << endl;
-//   cout << "IS_SINGLE: " << sizeof(IS_SINGLE) << endl;
-//   cout << "TYPE_ID: " << sizeof(Resource::TYPE_ID) << endl;
-//   cout << "DATA_T: " << sizeof(Resource::DATA_T) << endl;
-//   cout << "mutex: " << sizeof(mutex) << endl;
-//   cout << "std::unordered_map<ID_T, DATA_T>: " << sizeof(std::unordered_map<ID_T, Resource::DATA_T>) << endl;
-//   cout << "std::vector<DATA_T>: " << sizeof(std::vector<Resource::DATA_T>) << endl;
-//   cout << "DATA_VERIFIER_T: " << sizeof(Resource::DATA_VERIFIER_T) << endl;
-}
+using namespace wpp;
 
 void socketPolling(Connection *connection) {
 	while (true) {
@@ -42,26 +26,22 @@ void socketPolling(Connection *connection) {
 	}
 }
 
-int main()
-{
-	// cout << "Test memory consumption:" << endl;
-	// memConsumptionCheck();
-
+int main() {
 	cout << endl << "------------------- Creating requiered components -------------------" << endl;
-	Connection connection ("56830", AF_INET);
+	Connection connection("56830", AF_INET);
 	ServerImpl server;
 	SecurityImpl security;
 	DeviceImpl device;
 
 	// Client initialization
 	cout << endl << "------------------- Creating WppClient -------------------" << endl;
-	wpp::WppClient::create({"Test name", "", ""}, connection);
-	wpp::WppClient *client = wpp::WppClient::takeOwnership();
+	WppClient::create({"Test name", "", ""}, connection);
+	WppClient *client = WppClient::takeOwnership();
 	if (!client) {
 		cout << endl << "ERR: WppClient can not be taken" << endl;
 		return -1;
 	}
-	wpp::WppRegistry &registry = client->registry();
+	WppRegistry &registry = client->registry();
 
 	// Initialize wpp objects
 	cout << endl << "------------------- Initialization wpp Server -------------------" << endl;
@@ -75,23 +55,23 @@ int main()
 	client->giveOwnership();
 
 	cout << endl << "------------------- Starting Connection thread -------------------" << endl;
-	std::thread my_thread(socketPolling, &connection);
+	thread my_thread(socketPolling, &connection);
 
 	time_t callTime = 0;
 	for (int iterationCnt = 0; true; iterationCnt++) {
-		time_t currTime = std::time(NULL);
+		time_t currTime = time(NULL);
 
 		cout << endl << "------------------- iteration:" << iterationCnt << " -------------------" << endl;
 		if (currTime >= callTime || connection.getPacketQueueSize()) {
 			// Handle client state and process packets from the server
-			client = wpp::WppClient::takeOwnership();
+			client = WppClient::takeOwnership();
 			if (client) { 
 				callTime = currTime + client->loop();
 				client->giveOwnership();
-				cout << "Sleep time: " << callTime - std::time(NULL) << endl;
+				cout << "Sleep time: " << callTime - time(NULL) << endl;
 			}
 		}
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		this_thread::sleep_for(chrono::seconds(1));
 	}
 }
 

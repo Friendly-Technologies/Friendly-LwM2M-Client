0214bcbf0dd7291d1abb6739fcd4414d52a37387
Vadimzakreva <vadim@politex.org.ua>
2023-09-28 17:43:38 +0300

    avoid the double file updating

--- utils/object_code_generator.py
+++ utils/object_code_generator.py
@@ -432,18 +432,24 @@ class CodeGenerator:
     def update_file(self, stop_string, content, path_to_file):
         is_stop_string_present = False
         new_content = ''
+        old_content = ''
         with open(path_to_file, 'r') as f:
             for i in f:
-                # print(i)
-                if " ".join(i.split()) == stop_string:
-                    is_stop_string_present = True
-                    new_content += content
-
-                new_content += i
+                old_content += i
         f.close()
 
+        if old_content.find(content) != -1:
+            # print(f"File {path_to_file} is already updated")
+            return
+
+        for line in old_content.split("\n"):
+            if " ".join(line.split()) == stop_string:
+                is_stop_string_present = True
+                new_content += content
+            new_content += line + "\n"
+
         if not is_stop_string_present:
-            print(f"The {path_to_file} was not updated!")
+            # print(f"The {path_to_file} was not updated!")
             return
 
         with open(path_to_file, 'w') as f:

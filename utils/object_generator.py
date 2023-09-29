036b7e733cc148e55c035bd075373afb964fb5f5
Vadimzakreva <vadim@politex.org.ua>
2023-09-29 16:39:39 +0300

    GENERATOR: removed unused code

--- utils/object_generator.py
+++ utils/object_generator.py
@@ -139,17 +139,6 @@ FUNC_GET_INSTANTIATED_LIST_P = \
 
 class ObjectGenerator:
     """Add some comments here"""
-    meta_object = None
-    meta_resources = None
-
-    name_obj = None
-    name_obj_class = None
-    name_obj_define = None
-    name_obj_folder = None
-    name_obj_path_to_folder = None
-
-    name_object_info = None
-    name_camelcase = None
 
     def __init__(self, object_dict, resources_list):
         # meta data:
@@ -434,79 +423,9 @@ class ObjectGenerator:
             f"""#endif // {ifdef}\n"""
         return content
 
-    def update_file(self, stop_string, content, path_to_file):
-        is_stop_string_present = False
-        new_content = ''
-        old_content = ''
-        with open(path_to_file, 'r') as f:
-            for i in f:
-                old_content += i
-        f.close()
 
-        if old_content.find(content) != -1:
-            # print(f"File {path_to_file} is already updated")
-            return
 
-        for line in old_content.split("\n"):
-            if " ".join(line.split()) == stop_string:
-                is_stop_string_present = True
-                new_content += content
-            new_content += line + "\n"
 
-        if not is_stop_string_present:
-            # print(f"The {path_to_file} was not updated!")
-            return
-
-        with open(path_to_file, 'w') as f:
-            f.write(new_content[:-1])
-        f.close()
-
-    def update_files(self, object_dict):
-        is_obj_mandatory = object_dict["is_mandatory"]
-        stop_string_obj_id = STOP_STRING_OBJ_ID[0] if is_obj_mandatory else STOP_STRING_OBJ_ID[1]
-        stop_string_cfg_cmk = STOP_STRING_CNFG_CMK[0] if is_obj_mandatory else STOP_STRING_CNFG_CMK[1]
-
-        stop_string_reg_cpp = STOP_STRING_REG_PRT[0] if is_obj_mandatory else STOP_STRING_REG_PRT[1]
-        stop_string_reg_incl = STOP_STRING_REG_INCL[0] if is_obj_mandatory else STOP_STRING_REG_INCL[1]
-        stop_string_reg_prot = STOP_STRING_REG_PRT[0] if is_obj_mandatory else STOP_STRING_REG_PRT[1]
-
-        content_obj_id = \
-            f"#ifdef {self.name_obj_define}\n" \
-            f"\t{self.name_obj_class.upper()} = {object_dict['object_id']},\n" \
-            f"#endif /* {self.name_obj_define} */\n\n"
-
-        content_cnfg_cmk = \
-            f"""\noption({self.name_obj_define} """ \
-            f""""Include {"mandatory" if is_obj_mandatory else "optional"} """ \
-            f"""{self.name_obj_class} object in the build" {"ON" if is_obj_mandatory else "OFF"})\n""" \
-            f"""if ({self.name_obj_define})\n\tset(WPP_DEFINITIONS ${{WPP_DEFINITIONS}} {self.name_obj_define}=1)""" \
-            f"""\nendif()\n\n"""
-
-        content_reg_h_incl = \
-            f"""#if {self.name_obj_define}\n""" \
-            f"""#include "mandatory/{self.name_obj_folder}/{self.name_obj_class}.h"\n""" \
-            f"""#endif\n\n"""
-
-        content_reg_h_prt = \
-            f"""\t#if {self.name_obj_define}\n\t""" \
-            f"""{TYPE_OBJECT} <{self.name_obj_class}> & {self.name_camelcase}();\n\t""" \
-            f"""#endif\n\n"""
-
-        content_reg_cpp = \
-            f"""# if {self.name_obj_define}\n""" \
-            f"""{TYPE_OBJECT} <{self.name_obj_class}> & {TYPE_REGISTRY}::{self.name_camelcase}() {{\n\t""" \
-            f"""if (!{TYPE_OBJECT} <{self.name_obj_class}>::isCreated()) {TYPE_OBJECT} <{self.name_obj_class}>::create(_client, {self.name_obj_class.replace(' ', '').upper()}_OBJ_INFO);\n\t""" \
-            f"""return *{TYPE_OBJECT} <{self.name_obj_class}>::object();\n""" \
-            f"""}}\n""" \
-            f"""# endif\n"""
-
-        self.update_file(stop_string_obj_id, content_obj_id, "../wpp/registry/ObjectID.h")
-        self.update_file(stop_string_cfg_cmk, content_cnfg_cmk, "../wpp/config/config.cmake")
-
-        self.update_file(stop_string_reg_incl, content_reg_h_incl, "../wpp/registry/WppRegistry.h")
-        self.update_file(stop_string_reg_prot, content_reg_h_prt, "../wpp/registry/WppRegistry.h")
-
-        self.update_file(stop_string_reg_cpp, content_reg_cpp, "../wpp/registry/WppRegistry.cpp")
 
     def create_folder(self, folder_name):
         try:

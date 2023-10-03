6f4700dee1da18ee77349b43bd82f5a1165cbba4
Vadimzakreva <vadim@politex.org.ua>
2023-10-03 19:48:39 +0300

    CHANGER: added func that get user code blocks from files

--- utils/object_changer.py
+++ utils/object_changer.py
@@ -1,7 +1,31 @@
-from optparse import OptionParser
+import object_remover
 
-xp = object_xml_parser.ObjectXmlParser("./1-1_1.xml")
-obj_dict, res_list = xp.parse_xml()
-obj_names = xp.create_metadata()
-og = ObjectGenerator(obj_dict, res_list, obj_names)
-og.object_code_generate()
+from optparse import OptionParser
+import re
+
+
+def get_info(path_to_file):
+    add_line = False
+    counter = 0
+    user_code_block = ''
+    user_code_blocks = {}
+    with open(path_to_file, 'r') as f:
+        for line in f:
+            if re.search("block \d start", line):
+                add_line = True
+                continue
+
+            if re.search("block \d end", line):
+                add_line = False
+                user_code_blocks[counter] = user_code_block
+                counter += 1
+
+            if add_line:
+                user_code_block += line + "\n"
+    f.close()
+
+    return user_code_blocks
+
+
+# path_to_object = f"../wpp/registry/objects/mandatory/lwm2m_server_1_1"
+# obj_rem = object_remover.ObjectRemover(path_to_object)

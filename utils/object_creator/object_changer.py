b9661d9d4731cc2db5a2b24e141c2bbdfdd6f13f
VSkoshchuk <skoschuk999@gmail.com>
2023-10-13 17:53:09 +0300

    Implemented updater for user code

--- utils/object_creator/object_changer.py
+++ utils/object_creator/object_changer.py
@@ -8,29 +8,23 @@ import os
 
 keywords = ["FILETYPE_OBJ_IMPL_H", "FILETYPE_OBJ_IMPL_CPP", "FILETYPE_OBJ_CONF", "FILETYPE_OBJ_NFO"]
 
-RES_DEF_PATTERN = "#define RES_\d_\d_"
+RES_DEF_PATTERN = r'#define RES_\d+_\d+_'
 RES_DEF_PARTS_CNT = 3
 
-ENUM_FIELD_PATTERN = " = \d"
+ENUM_FIELD_PATTERN = r" = \d"
 ENUM_START_PATTERN = "enum ID: ID_T {"
 ENUM_END_PATTERN = "};"
 ENUM_FIELD_PARTS_CNT = 2
+
+FILE_TYPE_COMPONENTS_CNT = 2
 
 def get_info(path_to_file):
     add_line = False
     counter = 0
     user_code_block = ""
     user_code_blocks = {}
-    user_code_file_key = ""
-    user_code_file_key_found = False
     with open(path_to_file, 'r') as f:
         for line in f:
-            for keyword in keywords:
-                if not user_code_file_key_found:
-                    if re.search(keyword, line):
-                        user_code_file_key = keyword
-                        user_code_file_key_found = True
-
             if re.search("block \d start", line):
                 add_line = True
                 continue
@@ -45,7 +39,7 @@ def get_info(path_to_file):
                 user_code_block += line
     f.close()
 
-    return user_code_file_key, user_code_blocks
+    return user_code_blocks
 
 
 def put_info(path_to_file, file_user_code_dict):
@@ -81,40 +75,56 @@ def read_files(path):
     datas = {}
     dir_list = os.listdir(path)
     for file in dir_list:
-        file_keyword, user_codes = get_info(f"{path}/{file}")
-        datas[file_keyword] = user_codes
+        file_path = f"{path}/{file}"
+        file_type = get_file_type_by_path(file_path)
+        if not file_type:
+            print(f"Incorrect file type in '{file_path}'")
+            continue
+        user_codes = get_info(file_path)
+        datas[file_type] = user_codes
     return datas
 
 
 def write_files(folder_path, datas):
     dir_list = os.listdir(folder_path)
     for file in dir_list:
-        file_keyword = get_file_keyword(f"{folder_path}/{file}")
-        put_info(f"{folder_path}/{file}", datas[file_keyword])
+        file_path = f"{folder_path}/{file}"
+        file_type = get_file_type_by_path(file_path)
+        if not file_type:
+            print(f"Incorrect file type in '{file_path}'")
+            continue
+        put_info(file_path, datas[file_type])
 
 
-def get_file_keyword(file):
-    with open(file, 'r') as f:
-        for line in f:
-            for keyword in keywords:
-                if re.search(keyword, line):
-                    return keyword
-    f.close()
-
-
-def get_path_to_obj_file(obj_path, type):
+def get_obj_file_name_by_type(obj_path, type):
     files_list = os.listdir(obj_path)
     for file_name in files_list:
         with open(obj_path+"/"+file_name, 'r') as file:
             for line in file:
                 if re.search(type, line):
-                    return obj_path+"/"+file_name
+                    return file_name
+    return ""
+
+
+def get_file_type_by_path(obj_file_path):
+    with (open(obj_file_path, 'r') as file):
+        for line in file:
+            if not re.search(object_generator.FILE_TYPE_TAG, line):
+                continue
+            type_components = line.split(":")
+            if len(type_components) < FILE_TYPE_COMPONENTS_CNT:
+                break
+            file_type = type_components[1].replace(" ", "").strip("\t\n")
+            if file_type not in object_generator.FILE_TYPES:
+                break
+            return file_type
     return ""
 
 
 def get_resource_defines_from_file(obj_path):
-    cfg_file = get_path_to_obj_file(obj_path, "Object configuration")
+    cfg_file = get_obj_file_name_by_type(obj_path, object_generator.FILE_TYPE_OBJ_CFG)
     if not cfg_file:
+        print(f"File with type '{object_generator.FILE_TYPE_OBJ_CFG}' not found in the path '{obj_path}'")
         return dict()
 
     defines = dict()
@@ -134,14 +144,15 @@ def get_resource_defines_from_file(obj_path):
                 print(f"Incorrect define prefix format '{line_parts[1]}'")
                 continue
             defines[int(define_parts[2])] = line_parts[1]
+    return defines
 
 
 def get_obj_res_enum_content_from_file(obj_path):
-    impl_file = get_path_to_obj_file(obj_path, "Object implementation header")
+    impl_file = get_obj_file_name_by_type(obj_path, object_generator.FILE_TYPE_OBJ_IMPL_H)
     if not impl_file:
+        print(f"File with type '{object_generator.FILE_TYPE_OBJ_IMPL_H}' not found in the path '{obj_path}'")
         return ""
 
-    defines = dict()
     with open(obj_path + "/" + impl_file, 'r') as file:
         lines = file.readlines()
         enum_start_id = -1
@@ -161,39 +172,39 @@ def get_obj_res_enum_content_from_file(obj_path):
             block.append(lines[id])
         if enum_end_id == -1:
             return list()
-
-        return block
+    return block
 
 
 def get_resource_names_from_file(obj_path):
     # Return ["line0", "line1", ...]
     res_enum = get_obj_res_enum_content_from_file(obj_path)
     if not res_enum:
+        print("Enum with resources definitions not found")
         return dict()
 
     names = dict()
     for line in res_enum:
         if not re.search(ENUM_FIELD_PATTERN, line):
             continue
-        field_parts = line.strip("\t ,\n").split("=")
+        field_parts = line.replace(" ", "").strip("\t,\n").split("=")
         if len(field_parts) != ENUM_FIELD_PARTS_CNT or not field_parts[1].isnumeric():
             print(f"Incorrect enum field format '{line}'")
             continue
-
         names[int(field_parts[1])] = field_parts[0]
+    return names
 
 
 def get_resource_defines_from_gen(obj_gen):
     defines = dict()
     for res in obj_gen.meta_resources:
-        defines[res['ID']] = res['Define']
+        defines[int(res['ID'])] = res['Define']
     return defines
 
 
 def get_resource_names_from_gen(obj_gen):
     names = dict()
     for res in obj_gen.meta_resources:
-        names[res['ID']] = f"{res['Name']}_{res['ID']}"
+        names[int(res['ID'])] = f"{res['Name']}_{res['ID']}"
     return names
 
 
@@ -218,14 +229,15 @@ def get_updated_user_code(old_object_path, new_obj_gen):
 
     old_user_code_blocks = read_files(old_object_path)
     new_user_code_blocks = dict()
-    for blocks in old_user_code_blocks.values():
-        for id, old_block in blocks.items():
-            new_block = ""
+    for type, old_blocks in old_user_code_blocks.items():
+        new_user_code_blocks[type] = dict()
+        for id, old_block in old_blocks.items():
+            new_block = old_block
             for def_pair in res_defs_map:
-                new_block += old_block.replace(def_pair[0], def_pair[1])
+                new_block = new_block.replace(def_pair[0], def_pair[1])
             for name_pair in res_names_map:
-                new_block += old_block.replace(name_pair[0], name_pair[1])
-            new_user_code_blocks[id] = new_block
+                new_block = new_block.replace(name_pair[0], name_pair[1])
+            new_user_code_blocks[type][id] = new_block
     return new_user_code_blocks
 
 

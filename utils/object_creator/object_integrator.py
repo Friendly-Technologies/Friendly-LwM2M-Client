8ef926a076a0997500b5447a9596e956ae729098
Vadimzakreva <vadim@politex.org.ua>
2023-10-17 16:30:39 +0300

    updated previous commit: GENERATOR generate json file with metadata of the object but INTEGRATOR generates additional code again by using this json

--- utils/object_creator/object_integrator.py
+++ utils/object_creator/object_integrator.py
@@ -6,7 +6,19 @@ FOLDER_OBJECTS = "../../wpp/registry/objects"
 FOLDER_CONFIG = "../../wpp/config"
 FOLDER_REGISTRY = "../../wpp/registry"
 
-FILE_ADD_DATA = "additional.json"
+FILE_ADD_DATA = "obj_integration_data.json"
+
+STOP_STRING_CNFG_CMK = ["# The end of the options of the mandatory objects.",
+                        "# The end of the options of the optional objects."]
+STOP_STRING_OBJ_ID = ["/* The end of the IDs of the mandatory objects. */",
+                      "/* The end of the IDs of the optional objects. */"]
+STOP_STRING_REG_PRT = ["/* The end of the prototypes of the mandatory objects. */",
+                       "/* The end of the prototypes of the optional objects. */"]
+STOP_STRING_REG_INCL = ["/* The end of the includes of the mandatory objects. */",
+                        "/* The end of the includes of the optional objects. */"]
+
+TYPE_REGISTRY = "WppRegistry"
+TYPE_OBJECT = "Object"
 
 
 class ObjectIntegrator:
@@ -52,14 +64,57 @@ class ObjectIntegrator:
             return
 
         data_dict = eval(data_str)
+        dict_obj_meta = data_dict["obj_data"]
+        dict_obj_names = data_dict["obj_names"]
 
-        for i in data_dict:
-            stop_string = data_dict[i]["stop_string"]
-            content = data_dict[i]["content"]
-            file_name = data_dict[i]['file_name']
+        obj_is_mandatory = dict_obj_meta["object_is_mandatory"]
+        obj_id = dict_obj_meta["object_id"]
 
-            folder = FOLDER_CONFIG if file_name == "config.cmake" else FOLDER_REGISTRY
-            self.update_file(stop_string, content, f"{folder}/{file_name}")
+        obj_name_class = dict_obj_names["obj_name_class"]
+        obj_name_define = dict_obj_names["obj_name_define"]
+        obj_name_camelcase = dict_obj_names["obj_name_camelcase"]
+        obj_name_underline = dict_obj_names["obj_name_up_underline"]
+
+        stop_string_obj_id = STOP_STRING_OBJ_ID[0] if obj_is_mandatory else STOP_STRING_OBJ_ID[1]
+        stop_string_cfg_cmk = STOP_STRING_CNFG_CMK[0] if obj_is_mandatory else STOP_STRING_CNFG_CMK[1]
+        stop_string_reg_cpp = STOP_STRING_REG_PRT[0] if obj_is_mandatory else STOP_STRING_REG_PRT[1]
+        stop_string_reg_incl = STOP_STRING_REG_INCL[0] if obj_is_mandatory else STOP_STRING_REG_INCL[1]
+        stop_string_reg_prot = STOP_STRING_REG_PRT[0] if obj_is_mandatory else STOP_STRING_REG_PRT[1]
+
+        content_obj_id = \
+            f"#ifdef {obj_name_define}\n" \
+            f"\t{obj_name_underline} = {obj_id},\n" \
+            f"#endif /* {obj_name_define} */\n"
+
+        content_cfg_cmake = \
+            f"""option({obj_name_define} """ \
+            f""""Include {"mandatory" if obj_is_mandatory else "optional"} """ \
+            f"""{obj_name_class} object in the build" {"ON" if obj_is_mandatory else "OFF"})\n""" \
+            f"""if ({obj_name_define})\n\tset(WPP_DEFINITIONS ${{WPP_DEFINITIONS}} {obj_name_define}=1)""" \
+            f"""\nendif()\n"""
+
+        content_reg_h_include = \
+            f"""#if {obj_name_define}\n""" \
+            f"""#include "{self.folder_name}/{obj_name_class}.h"\n""" \
+            f"""#endif\n"""
+
+        content_reg_h_prototype = \
+            f"""\t#if {obj_name_define}\n\t""" \
+            f"""{TYPE_OBJECT} <{obj_name_class}> & {obj_name_camelcase}();\n\t""" \
+            f"""#endif\n"""
+
+        content_reg_cpp = \
+            f"""# if {obj_name_define}\n""" \
+            f"""{TYPE_OBJECT} <{obj_name_class}> & {TYPE_REGISTRY}::{obj_name_camelcase}() {{\n\t""" \
+            f"""if (!{TYPE_OBJECT} <{obj_name_class}>::isCreated()) {TYPE_OBJECT} <{obj_name_class}>::""" \
+            f"""create(_context, {obj_name_underline}_OBJ_INFO);\n\t""" \
+            f"""return *{TYPE_OBJECT} <{obj_name_class}>::object();\n}}\n# endif\n"""
+
+        self.update_file(stop_string_obj_id, content_obj_id, "../../wpp/registry/ObjectID.h")
+        self.update_file(stop_string_cfg_cmk, content_cfg_cmake, "../../wpp/config/config.cmake")
+        self.update_file(stop_string_reg_cpp, content_reg_cpp, "../../wpp/registry/WppRegistry.cpp")
+        self.update_file(stop_string_reg_incl, content_reg_h_include, "../../wpp/registry/WppRegistry.h")
+        self.update_file(stop_string_reg_prot, content_reg_h_prototype, "../../wpp/registry/WppRegistry.h")
 
     def remove_unused_files(self, path, file):
         try:

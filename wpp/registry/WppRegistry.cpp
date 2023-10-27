a5b623906a8cb1b0a02fee886250c53dcd764eec
Vadimzakreva <vadim@politex.org.ua>
2023-10-27 21:02:20 +0300

    Server: changed (updated)

--- wpp/registry/WppRegistry.cpp
+++ wpp/registry/WppRegistry.cpp
@@ -37,9 +37,9 @@ Object<Lwm2mSecurity> & WppRegistry::lwm2mSecurity() {
 }
 # endif
 # if OBJ_M_1_LWM2M_SERVER_V11
-Object <Lwm2mServer> & WppRegistry::lwm2mServer() {
-	if (!Object <Lwm2mServer>::isCreated()) Object <Lwm2mServer>::create(_context, LWM2M_SERVER_OBJ_INFO);
-	return *Object <Lwm2mServer>::object();
+Object<Lwm2mServer> & WppRegistry::lwm2mServer() {
+	if (!Object<Lwm2mServer>::isCreated()) Object<Lwm2mServer>::create(_context, LWM2M_SERVER_OBJ_INFO);
+	return *Object<Lwm2mServer>::object();
 }
 # endif
 /* The end of the prototypes of the mandatory objects. */

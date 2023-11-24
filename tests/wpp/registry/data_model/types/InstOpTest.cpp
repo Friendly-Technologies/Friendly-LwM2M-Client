97b864aa682aae89b52fa433dde8c9207bbd539d
Diachok_Viacheslav <slavko@sinai.io>
2023-11-24 18:58:16 +0200

    SN-1102 update tests for InstOp

--- tests/wpp/registry/data_model/types/InstOpTest.cpp
+++ tests/wpp/registry/data_model/types/InstOpTest.cpp
@@ -6,59 +6,58 @@ using namespace wpp;
 TEST_CASE("InstOp", "[InstOp]") {
     SECTION("Default Constructor") {
         InstOp op;
-        REQUIRE(op.getFlags()==0);
+
+        REQUIRE(op.getFlags() == 0 );
+        REQUIRE(op.isCompatible(InstOp::TYPE::CREATE));
 
         REQUIRE_FALSE(op.isSupported(InstOp::TYPE::NONE));
-
-        REQUIRE_FALSE(op.isSupported(InstOp::TYPE::CREATE));
-        REQUIRE_FALSE(op.isCreate());
-
-        REQUIRE_FALSE(op.isSupported(InstOp::TYPE::DELETE));
-        REQUIRE_FALSE(op.isDelete());
-
-        REQUIRE_FALSE(op.isSupported(InstOp::TYPE::BLOCK1_CREATE));
-        REQUIRE_FALSE(op.isBlockCreate());
-
-        REQUIRE(op.isCompatible(InstOp::TYPE::CREATE));
+        REQUIRE_FALSE(op.isSupported(InstOp::TYPE::CREATE)          & (op.isCreate()));
+        REQUIRE_FALSE(op.isSupported(InstOp::TYPE::DELETE)          & (op.isDelete()));
+        REQUIRE_FALSE(op.isSupported(InstOp::TYPE::BLOCK1_CREATE)   & (op.isBlockCreate()));
     }
 
     SECTION("NONE Function") {
         InstOp op(InstOp::TYPE::NONE);
-        REQUIRE(op.getFlags()==0);
+
+        REQUIRE(op.getFlags() == 0);
         REQUIRE_FALSE(op.isSupported(InstOp::TYPE::NONE));
     }
 
     SECTION("isCreate Function") {
         InstOp op(InstOp::TYPE::CREATE);
-        REQUIRE(op.getFlags()==1);
-
+        
         REQUIRE(op.isSupported(InstOp::TYPE::CREATE));
-        REQUIRE(op.isCreate());
+        REQUIRE(op.isCreate() & (op.getFlags() == 1));
 
-        REQUIRE_FALSE(op.isDelete());
-        REQUIRE_FALSE(op.isBlockCreate());
+        REQUIRE_FALSE(op.isDelete() & (op.isBlockCreate()));
     }
 
     SECTION("isDelete Function") {
         InstOp op(InstOp::TYPE::DELETE);
-        REQUIRE(op.getFlags()==2);
 
         REQUIRE(op.isSupported(InstOp::TYPE::DELETE));
-        REQUIRE(op.isDelete());
+        REQUIRE(op.isDelete() & (op.getFlags() == 2 ));
 
-        REQUIRE_FALSE(op.isCreate());
-        REQUIRE_FALSE(op.isBlockCreate());
+        REQUIRE_FALSE(op.isCreate() & (op.isBlockCreate()));
     }
 
     SECTION("isBlockCreate Function") {
         InstOp op(InstOp::TYPE::BLOCK1_CREATE);
-        REQUIRE(op.getFlags()==4);
 
         REQUIRE(op.isSupported(InstOp::TYPE::BLOCK1_CREATE));
-        REQUIRE(op.isBlockCreate());
+        REQUIRE(op.isBlockCreate() & (op.getFlags() == 4));
 
-        REQUIRE_FALSE(op.isCreate());
-        REQUIRE_FALSE(op.isDelete());
+        REQUIRE_FALSE(op.isCreate() & (op.isDelete()));
+    }
+
+    SECTION("getFlags Function") {
+        InstOp op(InstOp::TYPE::BLOCK1_CREATE | InstOp::TYPE::DELETE | InstOp::TYPE::CREATE);
+        REQUIRE(op.getFlags() == 7);
+    }
+
+    SECTION("getFlags with duplicates TYPE Function") {
+        InstOp op(InstOp::TYPE::BLOCK1_CREATE | InstOp::TYPE::BLOCK1_CREATE | InstOp::TYPE::BLOCK1_CREATE);
+        REQUIRE(op.getFlags() == 4);
     }
 
     SECTION("isCompatible Function") {

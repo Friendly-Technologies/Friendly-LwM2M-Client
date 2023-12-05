384c9cef2872b05de732d4b8d0ead88654119e3c
Diachok_Viacheslav <slavko@sinai.io>
2023-12-05 18:21:13 +0200

    SN-1102 corrected all comments /remove needless from .gitignore /added comment to the general CMakeLists /added tools for install to script /added examples files /added changes into the CMakeLists for testing process

---
+++ tests/Platform.cpp
@@ -0,0 +1,10 @@
+#include "WppPlatform.h"
+#include <ctime>
+
+time_t wpp::WppPlatform::getTime(void) {
+    return std::time(NULL);
+}
+
+void wpp::WppPlatform::print(const char * msg, va_list arg) {
+    vfprintf(stderr, msg, arg);
+}

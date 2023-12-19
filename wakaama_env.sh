e80d4f2516646dc3a09c4bcf92f86147d4927335
VSkoshchuk <skoschuk999@gmail.com>
2023-12-19 14:22:42 +0200

    Hardcoded build variant, set default path to cmake, updated env setup script

--- wakaama_env.sh
+++ wakaama_env.sh
@@ -31,9 +31,10 @@ clang-format-$CLANG_VERSION \
 clang-tools-$CLANG_VERSION \
 lldb-$CLANG_VERSION \
 $CLANG \
-cppcheck
-llvm
-libcurl4-openssl-dev"
+cppcheck \
+llvm \
+libcurl4-openssl-dev
+cmake"
 
 ptools="\
 cmake-format \

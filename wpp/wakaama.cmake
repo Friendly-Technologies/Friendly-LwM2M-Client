3f86c4d59f0fc7b984c73b90029d7f487200235b
VSkoshchuk <skoschuk999@gmail.com>
2023-12-20 14:09:09 +0200

    SN-1202 Refactored build system for wakaama core. Combined configs for wakaama and wpp.

--- wpp/wakaama.cmake
+++ wpp/wakaama.cmake
@@ -1,8 +1,10 @@
+# This file is used to build wakaama core library as part of other project
+# It is not used to build wakaama as standalone library, compilation
+# options and configs must be set in parent project
+
 set(WAKAAMA_CORE_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
 
-function(target_link_wakaama_core target)
-    # Include file with Wakaama core config
-    include(${WAKAAMA_CORE_DIRECTORY}/config/config.cmake)
+function(target_link_wakaama target)
     # Include file with wakaama sources config
     include(${WAKAAMA_CORE_DIRECTORY}/../2305-Wakaama/wakaama.cmake)
 
--- wpp/wpp.cmake
+++ wpp/wpp.cmake
@@ -1,20 +1,12 @@
 # This file is used to build wpp library as part of other project
 # It is not used to build wpp as standalone library, compilation
-# options must be set in parent project
+# options and configs must be set in parent project
 
 set(WPP_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
 
 function(target_link_wpp target)
-    # Variables that will be filled in subdirectories
-    set(WPP_SOURCES "")
-    set(WPP_INCLUDES "")
-    set(WPP_DEFINITIONS "")
-
-    # Include file with wpp config
-    include(${WPP_DIRECTORY}/configs/wpp_config.cmake)
-    include(${WPP_DIRECTORY}/configs/objects_config.cmake)
     # Include file with wakaama core config
-    include(${WPP_DIRECTORY}/../wakaama_core/WakaamaCore.cmake)
+    include(${WPP_DIRECTORY}/wakaama.cmake)
 
     # Add subdiretories with sources
     add_subdirectory(${WPP_DIRECTORY}/client client)
@@ -27,7 +19,7 @@ function(target_link_wpp target)
     # message(STATUS "Wpp:WPP_DEFINITIONS: ${WPP_DEFINITIONS}")
 
     # Link Wakaama that implements core functionality to Wpp
-    target_link_wakaama_core(${target})
+    target_link_wakaama(${target})
     # Add wakama core configs
     target_compile_definitions(${target} PUBLIC ${WPP_DEFINITIONS})
     # Include wakaama core sources

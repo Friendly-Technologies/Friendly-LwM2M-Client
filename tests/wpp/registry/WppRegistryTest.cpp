af3fe467c090017fb3e8cb4e0ba1593246ca0436
Diachok_Viacheslav <slavko@sinai.io>
2023-12-22 18:29:09 +0200

    SN-1203 modified considering wishes

--- tests/wpp/registry/WppRegistryTest.cpp
+++ tests/wpp/registry/WppRegistryTest.cpp
@@ -9,108 +9,99 @@ TEST_CASE("WppRegistry constructor creates objects", "[WppRegistry]")
     lwm2m_context_t context;
     // Create a WppRegistry instance
     WppRegistry WppRegistryInstance(context);
+    // Retrieve the objects created by the constructor
+    OBJ_ID objID;
 
-    SECTION("objId_func")
+    SECTION("objID")
     {
-        // Retrieve the objects created by the constructor
-        OBJ_ID objId;
+        REQUIRE(WppRegistryInstance.isObjExist(objID));
+        REQUIRE_FALSE(WppRegistryInstance.isObjExist(OBJ_ID(-1)));
 
-        const auto &createdObjects = WppRegistryInstance.isObjExist(objId);
-        const auto &objectObjects = WppRegistryInstance.object(objId);
-        const auto &objectNoObjects = WppRegistryInstance.object(OBJ_ID(25));
-
-        REQUIRE(createdObjects);
-        REQUIRE(objectObjects);
-        REQUIRE_FALSE(objectNoObjects);
+        REQUIRE(WppRegistryInstance.object(objID));
+        REQUIRE_FALSE(WppRegistryInstance.object(OBJ_ID(-1)));
     }
 
+#ifdef OBJ_M_3_DEVICE_V12
     SECTION("DEVICE")
     {
         ObjectSpec<Device> &obj = WppRegistryInstance.device();
 
-        const auto &registerObjects = WppRegistryInstance.registerObj(obj);
-        const auto &isObjRegistered = WppRegistryInstance.isObjRegistered(obj);
-        const auto &deregisterObjects = WppRegistryInstance.deregisterObj(obj);
-        const auto &isObjNoRegistered = WppRegistryInstance.isObjRegistered(obj);
-
-        REQUIRE(registerObjects);
-        REQUIRE(isObjRegistered);
-        REQUIRE(deregisterObjects);
-        REQUIRE_FALSE(isObjNoRegistered);
+        REQUIRE(obj.getObjectID() == OBJ_ID(3));
+        REQUIRE(WppRegistryInstance.object(OBJ_ID(3)));
+        REQUIRE(WppRegistryInstance.registerObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == true);
+        REQUIRE(WppRegistryInstance.deregisterObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == false);
     }
+#endif
 
+#ifdef OBJ_M_1_LWM2M_SERVER_V11
     SECTION("SERVER")
     {
         ObjectSpec<Lwm2mServer> &obj = WppRegistryInstance.lwm2mServer();
 
-        const auto &registerObjects = WppRegistryInstance.registerObj(obj);
-        const auto &isObjRegistered = WppRegistryInstance.isObjRegistered(obj);
-        const auto &deregisterObjects = WppRegistryInstance.deregisterObj(obj);
-        const auto &isObjNoRegistered = WppRegistryInstance.isObjRegistered(obj);
-
-        REQUIRE(registerObjects);
-        REQUIRE(isObjRegistered);
-        REQUIRE(deregisterObjects);
-        REQUIRE_FALSE(isObjNoRegistered);
+        REQUIRE(obj.getObjectID() == OBJ_ID(1));
+        REQUIRE(WppRegistryInstance.object(OBJ_ID(1)));
+        REQUIRE(WppRegistryInstance.registerObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == true);
+        REQUIRE(WppRegistryInstance.deregisterObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == false);
     }
+#endif
 
+#ifdef OBJ_M_0_LWM2M_SECURITY_V11
     SECTION("SECURITY")
     {
         ObjectSpec<Lwm2mSecurity> &obj = WppRegistryInstance.lwm2mSecurity();
 
-        const auto &registerObjects = WppRegistryInstance.registerObj(obj);
-        const auto &isObjRegistered = WppRegistryInstance.isObjRegistered(obj);
-        const auto &deregisterObjects = WppRegistryInstance.deregisterObj(obj);
-        const auto &isObjNoRegistered = WppRegistryInstance.isObjRegistered(obj);
-
-        REQUIRE(registerObjects);
-        REQUIRE(isObjRegistered);
-        REQUIRE(deregisterObjects);
-        REQUIRE_FALSE(isObjNoRegistered);
+        REQUIRE(obj.getObjectID() == OBJ_ID(0));
+        REQUIRE(WppRegistryInstance.object(OBJ_ID(0)));
+        REQUIRE(WppRegistryInstance.registerObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == true);
+        REQUIRE(WppRegistryInstance.deregisterObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == false);
     }
+#endif
 
+#ifdef OBJ_O_4_CONNECTIVITY_MONITORING_V13
     SECTION("CONNECTIVITY")
     {
         ObjectSpec<ConnectivityMonitoring> &obj = WppRegistryInstance.connectivityMonitoring();
 
-        const auto &registerObjects = WppRegistryInstance.registerObj(obj);
-        const auto &isObjRegistered = WppRegistryInstance.isObjRegistered(obj);
-        const auto &deregisterObjects = WppRegistryInstance.deregisterObj(obj);
-        const auto &isObjNoRegistered = WppRegistryInstance.isObjRegistered(obj);
-
-        REQUIRE(registerObjects);
-        REQUIRE(isObjRegistered);
-        REQUIRE(deregisterObjects);
-        REQUIRE_FALSE(isObjNoRegistered);
+        REQUIRE(obj.getObjectID() == OBJ_ID(4));
+        REQUIRE(WppRegistryInstance.object(OBJ_ID(4)));
+        REQUIRE(WppRegistryInstance.registerObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == true);
+        REQUIRE(WppRegistryInstance.deregisterObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == false);
     }
+#endif
 
+#ifdef OBJ_O_2_LWM2M_ACCESS_CONTROL_V11
     SECTION("ACCESS_CONTROL")
     {
         ObjectSpec<Lwm2mAccessControl> &obj = WppRegistryInstance.lwm2mAccessControl();
 
-        const auto &registerObjects = WppRegistryInstance.registerObj(obj);
-        const auto &isObjRegistered = WppRegistryInstance.isObjRegistered(obj);
-        const auto &deregisterObjects = WppRegistryInstance.deregisterObj(obj);
-        const auto &isObjNoRegistered = WppRegistryInstance.isObjRegistered(obj);
-
-        REQUIRE(registerObjects);
-        REQUIRE(isObjRegistered);
-        REQUIRE(deregisterObjects);
-        REQUIRE_FALSE(isObjNoRegistered);
+        REQUIRE(obj.getObjectID() == OBJ_ID(2));
+        REQUIRE(WppRegistryInstance.object(OBJ_ID(2)));
+        REQUIRE(WppRegistryInstance.registerObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == true);
+        REQUIRE(WppRegistryInstance.deregisterObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == false);
     }
+#endif
 
+#ifdef OBJ_O_5_FIRMWARE_UPDATE_V11
     SECTION("FIRMWARE_UPDATE")
     {
         ObjectSpec<FirmwareUpdate> &obj = WppRegistryInstance.firmwareUpdate();
 
-        const auto &registerObjects = WppRegistryInstance.registerObj(obj);
-        const auto &isObjRegistered = WppRegistryInstance.isObjRegistered(obj);
-        const auto &deregisterObjects = WppRegistryInstance.deregisterObj(obj);
-        const auto &isObjNoRegistered = WppRegistryInstance.isObjRegistered(obj);
-
-        REQUIRE(registerObjects);
-        REQUIRE(isObjRegistered);
-        REQUIRE(deregisterObjects);
-        REQUIRE_FALSE(isObjNoRegistered);
+        REQUIRE(obj.getObjectID() == OBJ_ID(5));
+        REQUIRE(WppRegistryInstance.object(OBJ_ID(5)));
+        REQUIRE(WppRegistryInstance.registerObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == true);
+        REQUIRE(WppRegistryInstance.deregisterObj(obj) == true);
+        REQUIRE(WppRegistryInstance.isObjRegistered(obj) == false);
     }
+#endif
 }

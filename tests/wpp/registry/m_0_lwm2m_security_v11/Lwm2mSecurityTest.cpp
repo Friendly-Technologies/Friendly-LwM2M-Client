84271705d25d0791c655611604f3486ca3b69918
Diachok_Viacheslav <slavko@sinai.io>
2024-01-01 12:16:48 +0200

    SN-1225 Lwm2mSecurity object test (mostly for nice coverage)

---
+++ tests/wpp/registry/m_0_lwm2m_security_v11/Lwm2mSecurityTest.cpp
@@ -0,0 +1,65 @@
+#include "catch_amalgamated.hpp"
+#include "./../../../../../wpp/registry/objects/m_0_lwm2m_security_v11/Lwm2mSecurity.h"
+
+#include "WppRegistry.h"
+#include "ObjectID.h"
+#include "Resource.h"
+#include "types.h"
+
+using namespace wpp;
+
+class Lwm2mSecurityMock : public Lwm2mSecurity
+{
+public:
+    Lwm2mSecurityMock(lwm2m_context_t &context, const OBJ_LINK_T &id) : Lwm2mSecurity(context, id) {}
+    void setDefaultState()
+    {
+        Lwm2mSecurity::setDefaultState();
+        Lwm2mSecurity::resource(LWM2M_SERVER_URI_0)->set(STRING_T("x!"));
+        Lwm2mSecurity::resource(SECURITY_MODE_2)->set(INT_T(LWM2M_SECURITY_MODE_NONE));
+        Lwm2mSecurity::resource(SHORT_SERVER_ID_10)->set(INT_T(0));
+    }
+
+    void serverOperationNotifier(ResOp::TYPE type, const ResLink &resId) { Lwm2mSecurity::serverOperationNotifier(type, resId); }
+    void userOperationNotifier(ResOp::TYPE type, const ResLink &resId) { Lwm2mSecurity::userOperationNotifier(type, resId); }
+};
+
+TEST_CASE("Lwm2mSecurity1", "[Lwm2mSecurity1]")
+{
+    SECTION("")
+    {
+        class Lwm2mSecurityMock : public Lwm2mSecurity
+        {
+        public:
+            Lwm2mSecurityMock(lwm2m_context_t &context, const OBJ_LINK_T &id) : Lwm2mSecurity(context, id) {}
+            void setDefaultState()
+            {
+                Lwm2mSecurity::setDefaultState();
+
+                REQUIRE(Lwm2mSecurity::resource(LWM2M_SERVER_URI_0)->set(STRING_T("x!")));
+                REQUIRE(Lwm2mSecurity::resource(SECURITY_MODE_2)->set(INT_T(LWM2M_SECURITY_MODE_NONE)));
+                REQUIRE(Lwm2mSecurity::resource(SECURITY_MODE_2)->set(INT_T(LWM2M_SECURITY_MODE_PRE_SHARED_KEY)));
+                REQUIRE(Lwm2mSecurity::resource(SHORT_SERVER_ID_10)->set(INT_T(1)));
+
+                REQUIRE_FALSE(Lwm2mSecurity::resource(LWM2M_SERVER_URI_0)->set(STRING_T("want-more-and-more-more-more1231231231231231231231231231231231231231231231231231231231231312312312312312312312312312312more1231231231231231231231231231231231231231231231231231231231231312312312312312312312312312312more1231231231231231231231231231231231231231231231231231231231231312312312312312312312312312312more1231231231231231231231231231231231231231231231231231231231231312312312312312312312312312312more1231231231231231231231231231231231231231231231231231231231231312312312312312312312312312312more1231231231231231231231231231231231231231231231231231231231231312312312312312312312312312312")));
+                REQUIRE_FALSE(Lwm2mSecurity::resource(SECURITY_MODE_2)->set(INT_T(-1)));
+                REQUIRE_FALSE(Lwm2mSecurity::resource(SECURITY_MODE_2)->set(INT_T(4)));
+                REQUIRE_FALSE(Lwm2mSecurity::resource(SHORT_SERVER_ID_10)->set(INT_T(0)));
+                REQUIRE_FALSE(Lwm2mSecurity::resource(SHORT_SERVER_ID_10)->set(INT_T(65535)));
+            }
+
+            void serverOperationNotifier(ResOp::TYPE type, const ResLink &resId) { Lwm2mSecurity::serverOperationNotifier(type, resId); }
+            void userOperationNotifier(ResOp::TYPE type, const ResLink &resId) { Lwm2mSecurity::userOperationNotifier(type, resId); }
+        };
+
+        lwm2m_context_t mockContext;
+        OBJ_LINK_T mockId = {0, 1};
+        // Create an instance of SecurityMock
+        Lwm2mSecurityMock securityMock(mockContext, mockId);
+
+        securityMock.setDefaultState();
+
+        securityMock.serverOperationNotifier(ResOp::TYPE::READ, {0, 0});
+        securityMock.userOperationNotifier(ResOp::TYPE::WRITE, {10, 10});
+    }
+}

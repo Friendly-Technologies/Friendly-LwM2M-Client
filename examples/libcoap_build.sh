c23b813ccc24b84555cb7b8504b999651f7be528
VSkoshchuk <skoschuk999@gmail.com>
2024-02-16 18:15:14 +0200

    Sn-1299 Updated coap build script

--- examples/libcoap_build.sh
+++ examples/libcoap_build.sh
@@ -3,25 +3,19 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Define the directory name
-REPO_DIR="libcoap"
+LOG_FILE="build.log"
+BUILD_DIR="build"
 
 # Remove the existing directory if it exists
-if [ -d "$REPO_DIR" ]; then
-    rm -rf "$REPO_DIR"
+if [ -d "$BUILD_DIR" ]; then
+    rm -rf "$BUILD_DIR"
 fi
 
-# Clone the libcoap repository
-git clone https://github.com/obgm/libcoap.git "$REPO_DIR"
-cd "$REPO_DIR"
-
 # Create a build directory
-BUILD_DIR="build"
 mkdir "$BUILD_DIR"
 cd "$BUILD_DIR"
 
 # Configure the build for a static library and append output to log file
-LOG_FILE="build.log"
 cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel -DENABLE_DTLS=ON -DENABLE_SERVER_MODE=OFF -DENABLE_EXAMPLES=OFF -DENABLE_DOCS=OFF >> "$LOG_FILE" 2>&1
 
 # Build the library and append output to the same log file

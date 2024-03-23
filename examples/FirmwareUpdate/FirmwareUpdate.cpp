ad7a38fdb24815a64f64f59f8b80ab72da53ed15
Vadimzakreva <vadim@politex.org.ua>
2024-03-23 16:03:59 +0200

    implemented pkgName and pkgVersion

--- examples/FirmwareUpdate/FirmwareUpdate.cpp
+++ examples/FirmwareUpdate/FirmwareUpdate.cpp
@@ -35,16 +35,37 @@ FwUpdRes FirmwareUpdateImpl::lastUpdateResult() {
     return FwUpdRes::R_FW_UPD_SUCCESS;
 }
 
+STRING_T read_metadata(uint8_t line_num) {
+    ifstream is;
+    string str = "default";
+   
+    cout << "FwUpdateImpl: read_metadata" << endl;
+
+    is.open("test_http.fw", ios::binary);
+    if (is.is_open()) {
+        // if file can't be opened it means it's not exists. Will return "default"
+        for (uint8_t i = 0; i < line_num; i++) {
+            getline(is, str);
+            // TODO: split by ":=" str
+        }
+    }
+    is.close();
+
+    return str;
+}
+
 #if RES_5_6
 STRING_T FirmwareUpdateImpl::pkgName() {
-    cout << "FwUpdateImpl: pkgName" << endl;
-    return "test";
+    string res = read_metadata(2);
+    cout << "FwUpdateImpl: pkgName: " << res << endl;
+    return res == "default" ? "current_fw" : res;
 }
 #endif
 
 #if RES_5_7
 STRING_T FirmwareUpdateImpl::pkgVersion() {
-    cout << "FwUpdateImpl: pkgVersion" << endl;
-    return "1.0";
+    string res = read_metadata(3);
+    cout << "FwUpdateImpl: pkgVersion: " << res << endl;
+    return res == "default" ? "1.0.0" : res;
 }
 #endif

11d1433accc8fa6db811225028f0b4ff74b4e1a8
VSkoshchuk <skoschuk999@gmail.com>
2024-04-02 13:15:44 +0300

    SN-1359 Created task for testing send operation

--- wpp/client/WppClient.cpp
+++ wpp/client/WppClient.cpp
@@ -148,7 +148,7 @@ void WppClient::deregister() {
 	lwm2m_deregister(_lwm2m_context);
 }
 
-#if RES_1_23
+#if defined(LWM2M_SUPPORT_SENML_JSON) && RES_1_23
 bool WppClient::send(const DataLink &link) {
 	WPP_LOGD(TAG_WPP_CLIENT, "Send data to servers: object ID -> %d, instance ID -> %d, resource ID -> %d, resource instance ID -> %d",
 				link.instance.objId, link.instance.objInstId, link.resource.resId, link.resource.resInstId);

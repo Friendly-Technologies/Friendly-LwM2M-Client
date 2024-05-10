a2dafa8135154b035790dfcb45a4d1ba502a7137
Diachok_Viacheslav <slavko@sinai.io>
2024-05-10 12:48:58 +0300

    SN-1379 changes

--- tests/wpp/registry/data_model/object/observer/ObjSubjectTest.cpp
+++ tests/wpp/registry/data_model/object/observer/ObjSubjectTest.cpp
@@ -1,55 +1,46 @@
 #include "catch_amalgamated.hpp"
 #include "ObjSubject.h"
 
-namespace wpp {
-class Object : public ObjSubject {
-public:
-    Object() {}
-    virtual ~Object() {}
+namespace wpp
+{
+    class Object : public ObjSubject
+    {
+    public:
+        Object() {}
+        virtual ~Object() {}
 
-    void operationNotify(Object &obj, ID_T instanceId, ItemOp::TYPE type) {
-        ObjSubject::operationNotify(obj, instanceId, type);
-    }
+        void operationNotify(Object &obj, ID_T instanceId, ItemOp::TYPE type) { ObjSubject::operationNotify(obj, instanceId, type); }
+    };
 
-    void observerDoAction(Object &obj, Action action) {
-        ObjSubject::observerDoAction(obj, action);
-    }
-};
+    class ObjOpObserverTest : public ObjOpObserver
+    {
+    public:
+        int instanceCreatedCount = 0;
+        int instanceDeletingCount = 0;
 
-class ObjOpObserverTest : public ObjOpObserver {
-public:
-    int instanceCreatedCount = 0;
-    int instanceDeletingCount = 0;
+        void instanceCreated(Object &object, ID_T instanceId) override
+        {
+            ObjOpObserver::instanceCreated(object, instanceId);
+            instanceCreatedCount++;
+        }
 
-    void instanceCreated(Object &object, ID_T instanceId) override {
-        ObjOpObserver::instanceCreated(object, instanceId);
-        instanceCreatedCount++;
-    }
-
-    void instanceDeleting(Object &object, ID_T instanceId) override {
-        ObjOpObserver::instanceDeleting(object, instanceId);
-        instanceDeletingCount++;
-    }
-};
-
-class ObjActObserverTest : public ObjActObserver {
-public:
-    int objectRestoreCount = 0;
-
-    void objectRestore(Object &object) override {
-        objectRestoreCount++;
-    }
-};
+        void instanceDeleting(Object &object, ID_T instanceId) override
+        {
+            ObjOpObserver::instanceDeleting(object, instanceId);
+            instanceDeletingCount++;
+        }
+    };
 }
 
 using namespace wpp;
 
-TEST_CASE("ObjSubject: subscribe/unsubscribe", "[opSubscribe][opUnsubscribe][actSubscribe][actUnsubscribe]") {
+TEST_CASE("ObjSubject: subscribe/unsubscribe", "[opSubscribe][opUnsubscribe][actSubscribe][actUnsubscribe]")
+{
     Object obj;
     ObjOpObserverTest opObserver;
-    ObjActObserverTest actObserver;
 
-    SECTION("Operation subscribe/unsubscribe") {
+    SECTION("Operation subscribe/unsubscribe")
+    {
         obj.opSubscribe(&opObserver);
         obj.opSubscribe(&opObserver);
         obj.opSubscribe(NULL);
@@ -57,7 +48,7 @@ TEST_CASE("ObjSubject: subscribe/unsubscribe", "[opSubscribe][opUnsubscribe][act
         REQUIRE(opObserver.instanceCreatedCount == 0);
         obj.operationNotify(obj, 0, ItemOp::CREATE);
         REQUIRE(opObserver.instanceCreatedCount == 1);
-         obj.operationNotify(obj, 0, ItemOp::CREATE);
+        obj.operationNotify(obj, 0, ItemOp::CREATE);
         REQUIRE(opObserver.instanceCreatedCount == 2);
 
         obj.opUnsubscribe(&opObserver);
@@ -65,29 +56,22 @@ TEST_CASE("ObjSubject: subscribe/unsubscribe", "[opSubscribe][opUnsubscribe][act
         REQUIRE(opObserver.instanceCreatedCount == 2);
     }
 
-    SECTION("Action subscribe/unsubscribe") {
-        obj.actSubscribe(&actObserver);
-        obj.actSubscribe(&actObserver);
-        obj.actSubscribe(NULL);
-
-        REQUIRE(actObserver.objectRestoreCount == 0);
-        obj.observerDoAction(obj, ObjSubject::RESTORE);
-        REQUIRE(actObserver.objectRestoreCount == 1);
-         obj.observerDoAction(obj, ObjSubject::RESTORE);
-        REQUIRE(actObserver.objectRestoreCount == 2);
-
-        obj.actUnsubscribe(&actObserver);
-        obj.observerDoAction(obj, ObjSubject::RESTORE);
-        REQUIRE(actObserver.objectRestoreCount == 2);
+    SECTION("Action subscribe/unsubscribe")
+    {
+        obj.opSubscribe(&opObserver);
+        obj.opSubscribe(&opObserver);
+        obj.opSubscribe(NULL);
+        obj.opUnsubscribe(&opObserver);
     }
 }
 
-TEST_CASE("ObjSubject: operation notify and request for action", "[operationNotify][observerDoAction]") {
+TEST_CASE("ObjSubject: operation notify and request for action", "[operationNotify][observerDoAction]")
+{
     Object obj;
     ObjOpObserverTest opObserver;
-    ObjActObserverTest actObserver;
 
-    SECTION("Operation notify") {
+    SECTION("Operation notify")
+    {
         obj.opSubscribe(&opObserver);
 
         REQUIRE(opObserver.instanceCreatedCount == 0);
@@ -104,41 +88,20 @@ TEST_CASE("ObjSubject: operation notify and request for action", "[operationNoti
         REQUIRE(opObserver.instanceCreatedCount == 2);
 
         ObjOpObserverTest opObservers[100];
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
+        {
             obj.opSubscribe(&opObservers[i]);
         }
 
         obj.operationNotify(obj, 0, ItemOp::CREATE);
         obj.operationNotify(obj, 0, ItemOp::CREATE);
         obj.operationNotify(obj, 0, ItemOp::DELETE);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
+        {
             REQUIRE(opObservers[i].instanceCreatedCount == 2);
             REQUIRE(opObservers[i].instanceDeletingCount == 1);
         }
         REQUIRE(opObserver.instanceCreatedCount == 4);
         REQUIRE(opObserver.instanceCreatedCount == 4);
     }
-
-    SECTION("Do action notify") {
-        obj.actSubscribe(&actObserver);
-
-        REQUIRE(actObserver.objectRestoreCount == 0);
-        obj.observerDoAction(obj, ObjSubject::RESTORE);
-        REQUIRE(actObserver.objectRestoreCount == 1);
-        obj.observerDoAction(obj, ObjSubject::RESTORE);
-        REQUIRE(actObserver.objectRestoreCount == 2);
-
-        ObjActObserverTest actObservers[100];
-        for (int i = 0; i < 100; i++) {
-            obj.actSubscribe(&actObservers[i]);
-        }
-
-        obj.observerDoAction(obj, ObjSubject::RESTORE);
-        obj.observerDoAction(obj, ObjSubject::RESTORE);
-        obj.observerDoAction(obj, ObjSubject::RESTORE);
-        for (int i = 0; i < 100; i++) {
-            REQUIRE(actObservers[i].objectRestoreCount == 3);
-        }
-        REQUIRE(actObserver.objectRestoreCount == 5);
-    }
-}   
+}
